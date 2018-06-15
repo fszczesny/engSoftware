4860b4ff9295b59dd4eec763d22b0f499d5e30e2
Rubens Rech <rubensch@icloud.com>
2018-06-15 19:45:01 -0300

    Improve employee home page

--- client/user/employee-home/employee-home.component.js
+++ client/user/employee-home/employee-home.component.js
@@ -4,9 +4,10 @@ angular.
     module('user').
     component('employeeHome', {
         templateUrl :'user/employee-home/employee-home.template.html',
-        controller:['UserService',
-                    '$scope',
-                    function EmployeeHome(UserService, $scope){
+        controller: ['UserService',
+                     '$scope',
+                     'EmployeeUserTypes',
+                     function EmployeeHome(UserService, $scope, EmployeeUserTypes) {
 
             var self = this;
 
@@ -14,6 +15,13 @@ angular.
             this.loggedIn = false;
             this.isAdmin = false;
             this.isManager = false;
+            this.userType = null;
+
+            var getUserType = function(userInfo) {
+                var userType = userInfo.userType;
+                var userTypeInfo = $.grep(EmployeeUserTypes, function(e) { return e.id == userType })[0];
+                return userTypeInfo.name;
+            };
 
             var loadUser = function() {
                 self.user = UserService.getUserData();
@@ -21,10 +29,10 @@ angular.
                 self.isAdmin = UserService.isAdmin();
                 self.isManager = UserService.isManager();
                 self.isEmployee = UserService.isEmployee();
+                self.userType = getUserType(self.user);
                 $scope.$applyAsync();
             };
             
             loadUser();
-            }
-        ]
+        }]
     });
